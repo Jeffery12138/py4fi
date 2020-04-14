6a2165b8524f27e628d58cb4363b0888f1da9a1f
Jeffery <1025958194@qq.com>
2020-04-14 21:35:31 +0800

    Financial Time

---
+++ code/ch08/Regression Analysis.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import os
+from pylab import plt, mpl
+
+plt.style.use('seaborn')
+mpl.rcParams['font.family'] = 'serif'
+
+fig_path = 'H:/py4fi/images/ch08/'
+if not os.path.exists(fig_path):
+    os.makedirs(fig_path)
+
+raw = pd.read_csv('tr_eikon_eod_data.csv', index_col=0, parse_dates=True)
+data = raw[['.SPX', '.VIX']].dropna()
+print(data.tail())
+# data.plot(subplots=True, figsize=(10, 6))
+# plt.savefig(fig_path+'fts_08.png')
+# data.loc[:'2012-12-31'].plot(secondary_y='.VIX', figsize=(10, 6))
+# plt.savefig(fig_path+'fts_09.png')
+# plt.show()
+print(data.loc[:'2012-12-31'].tail())
+rets = np.log(data/data.shift(1))
+print(rets.head())
+rets.dropna(inplace=True)
+rets.plot(subplots=True, figsize=(10, 6))
+# plt.savefig(fig_path+'fts_10.png')
+# pd.plotting.scatter_matrix(rets, alpha=0.2, diagonal='hist', hist_kwds={'bins':35}, figsize=(10, 6))  # alpha=0.2透明度
+# plt.savefig(fig_path+'fts_11.png')
+# pd.plotting.scatter_matrix(rets, alpha=0.2, diagonal='kde', figsize=(10, 6))
+# plt.show()
+reg = np.polyfit(rets['.SPX'], rets['.VIX'], deg=1)  # b1 b0 x, y  y=b1x+b0       线性回归，最小平方法
+ax = rets.plot(kind='scatter', x='.SPX', y='.VIX', figsize=(10, 6))
+ax.plot(rets['.SPX'], np.polyval(reg, rets['.SPX']), 'r', lw=2)
+plt.savefig(fig_path+'fts_12.png')
+# plt.show()
+print(rets.corr().iloc[0, 1])
+# ax = rets['.SPX'].rolling(window=252).corr(rets['.VIX']).plot(figsize=(10, 6))
+# ax.axhline(rets.corr().iloc[0, 1], c='r')
+ax = rets['.SPX'].rolling(window=252).corr(
+                  rets['.VIX']).plot(figsize=(10, 6))
+ax.axhline(rets.corr().iloc[0, 1], c='r')
+plt.savefig(fig_path+'fts_13.png')
+plt.show()
--- code/ch08/rolling statistics.py
+++ code/ch08/rolling statistics.py
@@ -22,10 +22,24 @@ data['std'] = data[sym].rolling(window=window).std()
 data['median'] = data[sym].rolling(window=window).median()
 data['max'] = data[sym].rolling(window=window).max()
 data['ewma'] = data[sym].ewm(halflife=0.5, min_periods=window).mean()  # 多种方法其实都是用来设置alpha
-print(data.dropna().head())
-ax = data[['min', 'mean', 'max']].iloc[-200:].plot(figsize=(10, 6), style=['g--', 'r--', 'g--'], lw=0.8)
+# print(data.dropna().head())
+# ax = data[['min', 'mean', 'max']].iloc[-200:].plot(figsize=(10, 6), style=['g--', 'r--', 'g--'], lw=0.8)
 # data[sym].iloc[-200:].plot(figsize=(10, 6), lw=2.0)  # .iloc 切片取行 .iloc[-200:]取最后200行
 # plt.show()
-data[sym].iloc[-200:].plot(ax=ax, lw=2.0)
-plt.savefig(fig_path+'fts_05.png')
+# data[sym].iloc[-200:].plot(ax=ax, lw=2.0)
+# plt.savefig(fig_path+'fts_05.png')
+# # plt.show()
+
+data['SMA1'] = data[sym].rolling(window=42).mean()
+data['SMA2'] = data[sym].rolling(window=252).mean()
+print(data[[sym, 'SMA1', 'SMA2']].tail())
+# data[[sym, 'SMA1', 'SMA2']].plot(figsize=(10, 6))
+# plt.savefig(fig_path+'fts_06.png')
+# plt.show()
+data.dropna(inplace=True)
+print(data)
+data['positions'] = np.where(data['SMA1']>data['SMA2'], 1, -1)
+ax = data[[sym, 'SMA1', 'SMA2', 'positions']].plot(figsize=(10, 6), secondary_y='positions')  # 第二个y轴 数据为'positions'
+ax.get_legend().set_bbox_to_anchor((0.25, 0.85))  # 设置图例位置
+plt.savefig(fig_path+'fts_07.png')
 plt.show()
